27279fa5fa75467dc428da8b69fe1abbce42e456
Nasti Turunen <nasti.turunen@gmail.com>
2024-10-01 12:57:37 +0300

    kommentteja, siivousta

--- käyttäjät/views.py
+++ käyttäjät/views.py
@@ -6,36 +6,39 @@ from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
 # Create your views here.
 
+#Sisäänkirjautuminen
 def kirjaudu_sisään(request):
     if request.method == "POST":
         nimi=request.POST["username"]
         salasana=request.POST["password"]
         käyttäjä=authenticate(request, username=nimi, password=salasana)
-        if käyttäjä is not None:
+        if käyttäjä is not None:#jos autentikointi onnistui
             login(request, käyttäjä)
             messages.success(request, "Olet nyt kirjautunut sisään!")
             return redirect("index")
         else:
-            messages.error(request, "Kokeile uudestaan")
+            messages.error(request, "Jotain meni pieleen, kokeile uudestaan")
             return render(request, "kirjaudu.html", {})
 
     return render(request, "kirjaudu.html", {})
 
+#Uloskirjautuminen
+@login_required
 def kirjaudu_ulos(request):
     logout(request)
     messages.success(request, "Kirjauduttu ulos")
     return redirect("index")
 
+#Uuden käyttäjätilin luonti
 def luo_tili(request):
     if request.method == "POST":
         form= UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Tilin luonti onnistui!")
             return redirect("index")
         else:
-            messages.error(request, "Kokeile uudelleen")
-            
-        
+            messages.error(request, "Jotain meni pieleen, kokeile uudelleen")       
     else:
         form= UserCreationForm()
     
@@ -45,19 +48,20 @@ def luo_tili(request):
 
     return render(request, "luo_tili.html", context)
 
+#Käyttäjän salasanan vaihto
 @login_required
 def salasana_vaihto(request):
     if request.method == "POST":
         form=PasswordChangeForm(request.user, request.POST)
         if form.is_valid():
             form.save()
-            update_session_auth_hash(request, form.user)
+            update_session_auth_hash(request, form.user) #ei tarvitse kirjautua uudelleen sisään
             messages.success(request, "Salasanan vaihto onnistui")
             return redirect("index")
         else:
-            messages.error(request, "kokeile uudelleen")
+            messages.error(request, "Jotain meni pieleen, kokeile uudelleen")
     else:
-        form=PasswordChangeForm(request.POST)
+        form=PasswordChangeForm(request.user)
 
     context={
         "form":form
--- ohjelma/views.py
+++ ohjelma/views.py
@@ -2,23 +2,25 @@ from django.shortcuts import render, redirect
 from .models import Työntekijä, Kunta, Maakunta, Työpiste
 from .forms import TyöntekijäForm
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+#from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 
 
 # Create your views here.
 
+#yleisnäkymä
 def index(request):
     työntekijät=Työntekijä.objects.all()
     kunnat=Kunta.objects.all().order_by('nimi')
     context={
         'tyontekijat':työntekijät,
         'kunnat':kunnat
-
     }
+
     return render(request, "index.html", context)
 
+#uuden työntekijän lisäys
 @login_required
 def lisää_työntekijä(request):
     if request.method == "POST":
@@ -40,7 +42,7 @@ def lisää_työntekijä(request):
     }
     return render(request, "tyontekija.html", context)
 
-            
+#Työntekijän tietojen muokkaus            
 @login_required
 def muokkaa_työntekijää(request, pk):
     työntekijä=Työntekijä.objects.get(pk=pk)
@@ -62,6 +64,7 @@ def muokkaa_työntekijää(request, pk):
     }
     return render(request, "tyontekija.html", context)
 
+#työntekijän poisto järjestelmästä
 @login_required
 def poista_työntekijä(request, pk):
     työntekijä=Työntekijä.objects.get(pk=pk)
@@ -69,6 +72,7 @@ def poista_työntekijä(request, pk):
     messages.success(request, "Työntekijä poistettu!")
     return redirect("index")
 
+#Kuntien haku valikkoa varten
 def lataa_kunnat(request):
     maakunta_id=request.GET.get('työmaakunta')
     kunnat=Kunta.objects.filter(maakunta_id=maakunta_id).order_by('nimi')
@@ -77,7 +81,7 @@ def lataa_kunnat(request):
     }
     return render(request, 'kunta_lista.html', context)
 
-
+#työpisteiden haku valikkoa varten
 def lataa_työpisteet(request):
     kunta_id=request.GET.get('työkunta')
     työpisteet=Työpiste.objects.filter(kunta_id=kunta_id).order_by('nimi')
@@ -86,6 +90,7 @@ def lataa_työpisteet(request):
     }
     return render(request, 'tyopiste_lista.html', context)
 
+#Työntekijöiden tietojen haku
 def hae_tyontekijat(request):
     tyontekijat=Työntekijä.objects.all().values('sukunimi',
                                                 'etunimi',
@@ -95,7 +100,7 @@ def hae_tyontekijat(request):
                                                 'työtehtävä',
                                                 'työkunta__nimi',
                                                 'työpiste__nimi',
-                                                'id')
+                                                'id')#haetaan työntekijöiden näytettävät tiedot(osoitetiedot jätetään hakematta)
     data=list(tyontekijat)
 
     return JsonResponse(data, safe=False)
